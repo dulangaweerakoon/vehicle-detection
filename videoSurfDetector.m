155b076a8463b6e82f56f1c42fb8d132d1f175a4
Dulanga KAVEESHA WEERAKOON <dulangakw@smu.edu.sg>
2016-10-11 14:48:37 +0800

    eDITED

--- videoSurfDetector.m
+++ videoSurfDetector.m
@@ -4,6 +4,7 @@ videoPlayer = vision.DeployableVideoPlayer();
 oldframe = rgb2gray(reader.step());
 frame = oldframe;
 
+load('ROI.mat','ROI');
 while ~isDone(reader)
     oldframe = frame;
     frame = reader.step();
@@ -18,9 +19,23 @@ while ~isDone(reader)
     %imshow(frame);
     %hold on
     %plot(pts.selectStrongest(100))
-    a = pts.selectStrongest(50);
-    position = [a.Location,5*ones(50,1)];
-    label = ones(1,50);
+    cc = pts.selectStrongest(50);
+    cc
+    locations = cc.Location;
+    laplacian = cc.Scale;
+    
+    position = zeros(0,3);
+    label = ones(1,0);
+    for i=1:length(locations)
+        if ROI(floor(locations(i,2)),floor(locations(i,1)) )
+                label = [label ; laplacian(i)];
+                position = [position ; [locations(i,:),5]];
+                %bboxes = [bboxes;[(cc(i,1)-5),(cc(i,2)-5),10,10]];
+        end
+    end 
+    
+    %position = [cc.Location,5*ones(50,1)];
+    %label = ones(1,length(position));
     
     RGB = insertObjectAnnotation(frame, 'circle', position, label, ...
       'LineWidth', 3,  'TextColor', 'black');
